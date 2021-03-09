11726716d4357918966b054b76cdf830c4d31828
AmitBashari <bashari.amit@gmail.com>
2021-03-09 19:20:58 +0200

    HealthBars(NotSyncedYet)

---
+++ Assets/CoopTowerDefense/MyScripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int MaxHealth = 100;
+    public int CurrentHealth = MaxHealth;
+    public RectTransform HealthBar;
+
+    public void GetWet(int amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Debug.Log("Fully Watered!");
+        }
+
+        HealthBar.sizeDelta = new Vector2(CurrentHealth * 2, HealthBar.sizeDelta.y);
+    }
+
+}
---
+++ Assets/CoopTowerDefense/MyScripts/WaterBaloon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBaloon : MonoBehaviour
+{
+    private int _wetAmount = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        Health health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.GetWet(_wetAmount);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
